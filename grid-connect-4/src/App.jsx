670d7e5c63e07dad438a3cbec7b4336f3bfc7033
Manish Mohan <mohanmanis@gmail.com>
2024-05-17 19:01:31 +0530

    Added flex version

--- grid-connect-4/src/App.jsx
+++ grid-connect-4/src/App.jsx
@@ -1,8 +1,20 @@
 import GridUsingCssGrid from "./grid-using-css-grid/Grid";
+import GridUsingCssFlex from "./grid-using-css-flex/Grid";
 import "./App.css";
 
 function App() {
-  return <GridUsingCssGrid rows={5} cols={5} />;
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div>
+        <h1>Using CSS Grid</h1>
+        <GridUsingCssGrid rows={5} cols={5} />
+      </div>
+      <div>
+        <h1>Using CSS Flex</h1>
+        <GridUsingCssFlex rows={5} cols={5} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
